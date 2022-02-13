72f908ba04b7b10bfb53dfc3b2c71274609cd4ec
arifphero <ahammadarif232@gmail.com>
2022-02-13 22:20:53 +0600

    step -2 added

---
+++ js/event.js
@@ -0,0 +1,10 @@
+/* step - 1 */
+//write the code inside the tag.
+
+/* step - 2 */
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+  document.getElementById("dom-title").style.color = "white";
+}
+
+/* step - 3 */
